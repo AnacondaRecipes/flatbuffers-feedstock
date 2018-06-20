4afb28b8fad9908f3b373993a25469f73b0a310f
Michael Sarahan <msarahan@gmail.com>
2018-06-19 21:40:17 -0500

    cb3-ize

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,21 +2,33 @@
 
 set -e
 
-# C++11 finagling
-if [ "$(uname)" == "Darwin" ]; then
-  # for Mac OSX
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_VERSION_MIN="10.7"
-  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
-  export LINKFLAGS="${LDFLAGS}"
-  export MACOSX_DEPLOYMENT_TARGET=10.7
+declare -a _CMAKE_EXTRA_CONFIG
+if [[ ${HOST} =~ .*darwin.* ]]; then
+    if [[ ${_XCODE_BUILD} == yes ]]; then
+        _CMAKE_EXTRA_CONFIG+=(-G'Xcode')
+        _CMAKE_EXTRA_CONFIG+=(-DCMAKE_OSX_ARCHITECTURES=x86_64)
+        _CMAKE_EXTRA_CONFIG+=(-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT})
+        _VERBOSE=""
+    fi
+    unset MACOSX_DEPLOYMENT_TARGET
+    export MACOSX_DEPLOYMENT_TARGET
+    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_AR=${AR})
+    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_RANLIB=${RANLIB})
+    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_LINKER=${CXX})
+fi
+if [[ ${HOST} =~ .*linux.* ]]; then
+    # I hate you so much CMake.
+    LIBPTHREAD=$(find ${PREFIX} -name "libpthread.so")
+    _CMAKE_EXTRA_CONFIG+=(-DPTHREAD_LIBRARY=${LIBPTHREAD})
 fi
 
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+      -DCMAKE_C_COMPILER=$(type -p ${CC})     \
+      -DCMAKE_CXX_COMPILER=$(type -p ${CXX})  \
+      "${_CMAKE_EXTRA_CONFIG[@]}" \
+      ..
 make
 ctest
 make install
